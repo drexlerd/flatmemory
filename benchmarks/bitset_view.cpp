5167159d3626c13879ba21e3d038500d74320b17
Dominik Drexler <dominik.drexler@gmx.net>
2024-08-26 19:04:52 +0200

    fix benchmark compile error

--- benchmarks/bitset_view.cpp
+++ benchmarks/bitset_view.cpp
@@ -15,76 +15,78 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <flatmemory/flatmemory.hpp>
-
 #include <benchmark/benchmark.h>
-
+#include <flatmemory/flatmemory.hpp>
 #include <iostream>
 #include <random>
 
-
-namespace flatmemory::benchmarks 
+namespace flatmemory::benchmarks
 {
-    static auto create_views(
-        const size_t num_bitsets,
-        const size_t bitset_size) {
-        // Create num_bitset many views
-        VariableSizedTypeVector<Bitset<uint64_t>> views;
-        auto builder = Builder<Bitset<uint64_t>>();
-        builder.get_blocks().resize(bitset_size);
-        for (size_t i = 0; i < num_bitsets; ++i) {
-            builder.finish();
-            views.push_back(builder);
-        }
-        return views;
+static auto create_views(const size_t num_bitsets, const size_t bitset_size)
+{
+    // Create num_bitset many views
+    VariableSizedTypeVector<Bitset<uint64_t>> views;
+    auto builder = Builder<Bitset<uint64_t>>();
+    builder.get_blocks().resize(bitset_size);
+    for (size_t i = 0; i < num_bitsets; ++i)
+    {
+        builder.finish();
+        views.push_back(builder);
     }
+    return views;
+}
 
-    static void BM_BitsetViewCreate(benchmark::State& state) {
-        const size_t num_bitsets = state.range(0);
-        const size_t bitset_size = state.range(1);
+static void BM_BitsetViewCreate(benchmark::State& state)
+{
+    const size_t num_bitsets = state.range(0);
+    const size_t bitset_size = state.range(1);
 
-        for (auto _ : state) {
-            auto views = create_views(num_bitsets, bitset_size);
-
-            benchmark::DoNotOptimize(views);
-        }
-    }
-
-    static void BM_BitsetViewAccess(benchmark::State& state) {
-        const size_t num_bitsets = state.range(0);
-        const size_t bitset_size = state.range(1);
-
+    for (auto _ : state)
+    {
         auto views = create_views(num_bitsets, bitset_size);
 
-        std::random_device rd;  // Obtain a random number from hardware
-        std::mt19937 eng(rd()); // Seed the generator
+        benchmark::DoNotOptimize(views);
+    }
+}
 
-        for (auto _ : state) {
-            std::vector<int> indices(views.size());
-            std::iota(indices.begin(), indices.end(), 0);
-            std::shuffle(indices.begin(), indices.end(), eng);
+static void BM_BitsetViewAccess(benchmark::State& state)
+{
+    const size_t num_bitsets = state.range(0);
+    const size_t bitset_size = state.range(1);
 
-            for (int index : indices) {
-                auto view = views[index];
+    auto views = create_views(num_bitsets, bitset_size);
 
-                auto& default_bit_value = view.get_default_bit_value();
-                benchmark::DoNotOptimize(default_bit_value);
-                
-                for (auto block : view.get_blocks()) {
-                    benchmark::DoNotOptimize(block);
-                }
+    std::random_device rd;   // Obtain a random number from hardware
+    std::mt19937 eng(rd());  // Seed the generator
+
+    for (auto _ : state)
+    {
+        std::vector<int> indices(views.size());
+        std::iota(indices.begin(), indices.end(), 0);
+        std::shuffle(indices.begin(), indices.end(), eng);
+
+        for (int index : indices)
+        {
+            auto view = views[index];
+
+            auto default_bit_value = view.get_default_bit_value();
+            benchmark::DoNotOptimize(default_bit_value);
+
+            for (auto block : view.get_blocks())
+            {
+                benchmark::DoNotOptimize(block);
             }
         }
     }
 }
+}
 
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({ 1000000, 1 });
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({ 1000000, 10 });
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({ 1000000, 100 });
 
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({1000000, 1});
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({1000000, 10});
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewCreate)->Args({1000000, 100});
-
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({1000000, 1});
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({1000000, 10});
-BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({1000000, 100});
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({ 1000000, 1 });
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({ 1000000, 10 });
+BENCHMARK(flatmemory::benchmarks::BM_BitsetViewAccess)->Args({ 1000000, 100 });
 
 BENCHMARK_MAIN();
