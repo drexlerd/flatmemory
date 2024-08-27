c39b4c4fa8356666ec1e6485deee036ecfbafb18
Dominik Drexler <dominik.drexler@gmx.net>
2024-08-27 15:08:30 +0200

    added missing include

--- include/flatmemory/details/types/optional.hpp
+++ include/flatmemory/details/types/optional.hpp
@@ -26,6 +26,7 @@
 #include "flatmemory/details/concepts.hpp"
 #include "flatmemory/details/layout.hpp"
 #include "flatmemory/details/types.hpp"
+#include "flatmemory/details/types/declarations.hpp"
 #include "flatmemory/details/view.hpp"
 #include "flatmemory/details/view_const.hpp"
 
